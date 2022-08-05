9176aa0d2cc821a0aeb12aec03548e863118540a
ErJiQwQ <2776483968@qq.com>
2022-08-05 15:32:35 +0800

    购物车

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    购物车
+    <!--顶部编辑栏-->
+    <div class="cart-header">
+      <van-icon name="editor"></van-icon>
+      <span>编辑</span>
+    </div>
+
   </div>
 </template>
 
@@ -11,5 +16,10 @@ export default {
 </script>
 
 <style>
-
+.cart-header{
+  text-align: right;
+  height: 46px;
+  line-height: 46px;
+  margin-right: 10px;
+}
 </style>
